617ecb9e45b2aaaa28dd4d08cad8a0ec92585e9a
Lictral <lguillermont@gaming.tech>
2025-11-12 09:05:41 +0100

    [FIX] 2.1

--- ProjetMath/ProjetMath/Mesh.cpp
+++ ProjetMath/ProjetMath/Mesh.cpp
@@ -1,1 +1,23 @@
 #include "Mesh.h"
+
+Mesh::Mesh()
+{
+
+}
+
+Mesh::~Mesh()
+{
+
+}
+
+void Mesh::AddVertex(float x, float y, float z)
+{
+	m_vertex.push_back(x);
+	m_vertex.push_back(y);
+	m_vertex.push_back(z);
+}
+
+void Mesh::Debug()
+{
+
+}
--- ProjetMath/ProjetMath/Mesh.h
+++ ProjetMath/ProjetMath/Mesh.h
@@ -8,5 +8,12 @@ public:
 	~Mesh();
 
 	void Debug();
+	void AddVertex(float x, float y, float z);
+	void CreateTriangle();
+	void CreateSquare();
+
+private:
+	std::vector<float> m_vertex;
+	std::vector<float> m_allVertex;
 };
 
